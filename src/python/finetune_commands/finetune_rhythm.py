18218c4b05a8136483292b3293faf9fb8c310364
satishnagc <33736095+satishnagc@users.noreply.github.com>
2024-05-03 22:47:47 -0500

    Update finetune_rhythm.py

--- src/python/finetune_commands/finetune_rhythm.py
+++ src/python/finetune_commands/finetune_rhythm.py
@@ -1,16 +1,9 @@
-from pretraining.utils import get_pretrained_weights
-
-
-resnet18 = get_pretrained_weights(
-   checkpoint_file='jobs/rhythm_classification/epoch_01_model.weights.h5',
-   task='rhythm',
-   arch='resnet18')
-resnet18.save_weights('jobs/rhythm_classification/resnet18.weights.h5')
-
-
-
-resnet34 = get_pretrained_weights(
-   checkpoint_file='jobs/rhythm_multiple_epochs_34_classification/epoch_10_model.weights.h5',
-   task='rhythm',
-   arch='resnet34')
-resnet34.save_weights('jobs/rhythm_classification/resnet34.weights.h5')
+python -m finetuning.trainer \
+--job-dir "jobs/rhythm_classification" \
+--train "data/physionet_train.pkl" \
+--test "data/physionet_test.pkl" \
+--weights-file "jobs/rhythm_classification/resnet18.weights.h5" \
+--val-size 0.0625 \
+--arch "resnet18" \
+--batch-size 64 \
+--epochs 200
