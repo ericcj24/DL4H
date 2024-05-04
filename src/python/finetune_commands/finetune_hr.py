3f9e6cbe01bf8684e5c224534f559c7d076f18ba
satishnagc <33736095+satishnagc@users.noreply.github.com>
2024-05-03 22:48:09 -0500

    Update finetune_hr.py

--- src/python/finetune_commands/finetune_hr.py
+++ src/python/finetune_commands/finetune_hr.py
@@ -1,15 +1,9 @@
-from pretraining.utils import get_pretrained_weights
-
-resnet18 = get_pretrained_weights(
-   checkpoint_file='jobs/hr_classification/epoch_01_model.weights.h5',
-   task='hr',
-   arch='resnet18')
-resnet18.save_weights('jobs/hr_classification/resnet18.weights.h5')
-
-
-
-resnet34 = get_pretrained_weights(
-   checkpoint_file='jobs/hr_multiple_epochs_34_classification/epoch_09_model.weights.h5',
-   task='hr',
-   arch='resnet34')
-resnet34.save_weights('jobs/hr_classification/resnet34.weights.h5')
+python -m finetuning.trainer \
+--job-dir "jobs/hr_classification" \
+--train "data/physionet_train.pkl" \
+--test "data/physionet_test.pkl" \
+--weights-file "jobs/hr_classification/resnet18.weights.h5" \
+--val-size 0.0625 \
+--arch "resnet18" \
+--batch-size 64 \
+--epochs 200
